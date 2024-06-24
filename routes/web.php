3b77cd3a8895e87db2e866fe1f0ed2aea54e1791
mishkat01 <mishkatislam007@gmail.com>
2024-06-24 18:02:58 +0600

    route mng

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\InputController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('inputs', InputController::class);
